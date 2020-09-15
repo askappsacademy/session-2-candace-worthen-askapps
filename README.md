a3100cea3bd94127d6a8b54ce8ee8f16ca633f22
andylu821 <51221593+andylu821@users.noreply.github.com>
2020-09-15 18:06:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ask Apps Academy Session 2
+
+## Session 1 exercise prerequisites:
+1. Register and sign into Github account: https://github.com
+1. Authorize and accept Github Classroom assignment 2: https://classroom.github.com/a/0GqUmk_t
+1. Register and open repo in Repl.it online IDE by import from Github option: https://repl.it/
+
+## Session 2 instructions:
+### In this exercise, we will be using the video player application code from session 1 and add functionality to it with Javascript that we've learned in class
+
+#### Session 2 exercise 1 - JS
+1. Link the videoPlayer.js file using the script tag in index.html file
+1. Follow the rest of the instructions in videoPlayer.js to complete the exercise
+
+## License 
+© 2020 Ask Applications, Inc. an IAC Company. All rights reserved
---
+++ videoPlayer.js
@@ -0,0 +1,16 @@
+// array defined to map the video urls to the video elements in index.html file
+const videoArray = [
+    "https://player.vimeo.com/video/35055590?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=147673",
+    "https://player.vimeo.com/video/163662857?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=147673",
+    "https://player.vimeo.com/video/60735314?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=147673",
+    "https://player.vimeo.com/video/81229520?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=147673",
+    "https://player.vimeo.com/video/229651971?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=147673"
+];
+
+// task 1 - declare a var to target video play list element
+
+// task 2 - write a function setVideos where it loops through the video play list elements in index.html file then apply click event handler to pass url from videoArray to another function
+
+// task 3 - write a function loadVideo where it takes an url as a parameter to render the video as an iframe in the video container element
+
+// task 4 - call setVideos function to pass in the above video play list element
